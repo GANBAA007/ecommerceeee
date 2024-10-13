7ff040af0d544c814c2b43927f8a40d2d88f8342
GANBAA007 <invisinvin87@gmail.com>
2024-10-13 18:19:25 +0800

    database completed

--- models/products.go
+++ models/products.go
@@ -4,11 +4,11 @@ import (
 	"gorm.io/gorm"
 )
 
-type product struct {
+type Product struct {
 	gorm.Model
 	Name        string      `gorm:"not null"`
 	Description string      `gorm:"type:text"`
 	Price       float64     `gorm:"not null"`
 	Stock       int         `gorm:"not null"`
-	OrderItems  []OrderItem `gorm:"foreignKey:productID"` // One-to-many relationship with OrderItem
+	OrderItems  []OrderItem `gorm:"foreignKey:ProductID"` // One-to-many relationship with OrderItem
 }
